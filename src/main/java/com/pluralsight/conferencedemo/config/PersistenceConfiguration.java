73d6c228588c54032db9310b27d50743ba6f16dd
Robert Pratte <pic2731@gmail.com>
2022-10-11 14:16:45 -0400

    Commenting out bean

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -10,15 +10,13 @@ import static java.lang.System.getenv;
 
 @Configuration
 public class PersistenceConfiguration {
-
-
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username(System.getenv("DB_USR"));
-        builder.password(System.getenv("DB_PASS"));
-        System.out.println("My custom datasource bean has been initialzed and set");
-        return builder.build();
-    }
+    //  @Bean
+    //  public DataSource dataSource() {
+    //        DataSourceBuilder builder = DataSourceBuilder.create();
+    //        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+    //        builder.username(System.getenv("DB_USR"));
+    //        builder.password(System.getenv("DB_PASS"));
+    //        System.out.println("My custom datasource bean has been initialzed and set");
+    //        return builder.build();
+    //    }
 }
